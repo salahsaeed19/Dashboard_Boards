913bb2ebe69d25f65b46d6132d46fa0c7d3ddfe3
salahsaeed19 <salahsaef40@gmail.com>
2023-08-25 21:52:46 +0300

    Add Models (3 Tables)

--- boards/models.py
+++ boards/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=500)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=100)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+    created_dt = models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=5000)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_dt = models.DateTimeField(auto_now_add=True)
